621beab07b7ec5957c780053adb90e9a418b00ba
thomasjohnson0426 <thomasjohnson0426@gmail.com>
2025-05-19 04:34:12 -0500

    update configuration and e2e test

--- src/core/configs/configuration.ts
+++ src/core/configs/configuration.ts
@@ -1,7 +1,5 @@
-const PORT = process.env.PORT || '5000';
-
 export default () => ({
-  port: parseInt(PORT, 10) || 5000,
+  port: parseInt(process.env.PORT!, 10) || 5000,
   mongoUri: process.env.MONGO_URI,
   redis: { uri: process.env.REDIS_URI },
   queueName: process.env.QUEUE_NAME,
